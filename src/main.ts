a369c715c87bfc34bd5a3ac11d6c38498d7cb755
ngfchl <ngfchl@126.com>
2023-03-15 14:53:23 +0800

    添加 pinia状态管理

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 import '@unocss/reset/tailwind.css'
 import 'uno.css'
 import router from '~/routes'
 
+const pinia = createPinia()
 /**
  * 解决tailwind的样式冲突
  */
@@ -12,6 +14,6 @@ meta.name = 'naive-ui-style'
 document.head.appendChild(meta)
 
 const app = createApp(App)
-
+app.use(pinia)
 app.use(router)
 createApp(App).mount('#app')
