5048cbca3ec5a884e9a73abbda10d21995d6efc2
ngfchl <ngfchl@126.com>
2023-03-15 17:36:38 +0800

    完成基础布局路由映射

---
+++ src/layouts/base-layout/index.vue
@@ -0,0 +1,9 @@
+<script>
+export default {
+  name: 'Index',
+}
+</script>
+
+<template>
+  <router-view />
+</template>
--- src/routes/static-routes.ts
+++ src/routes/static-routes.ts
@@ -1,16 +1,27 @@
 import type { RouteRecordRaw } from 'vue-router'
+import { Layout } from '~/layouts'
 
 const staticRoutes: RouteRecordRaw[] = [
   {
     path: '/',
-    component: () => import('~/pages/index.vue'),
     name: 'index',
+    component: Layout,
+    redirect: '/home',
+    children: [
+      {
+        path: '/home',
+        component: () => import('~/pages/index.vue'),
+        name: 'index',
+      },
+      {
+        path: '/workspace',
+        component: () => import('~/pages/workspace/index.vue'),
+        name: 'workspace',
+      },
+    ],
+
   },
-  {
-    path: '/workspace',
-    component: () => import('~/pages/workspace/index.vue'),
-    name: 'workspace',
-  },
+
 ]
 
 export default staticRoutes
