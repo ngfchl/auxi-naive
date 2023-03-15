7c3d89e75d4696a6bcdcc4b1a82849ee095c92bd
ngfchl <ngfchl@126.com>
2023-03-15 10:59:15 +0800

    安装配置 unocss

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,14 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import '@unocss/reset/tailwind.css'
+import 'uno.css'
+
+/**
+ * 解决tailwind的样式冲突
+ */
+const meta = document.createElement('meta')
+meta.name = 'naive-ui-style'
+document.head.appendChild(meta)
 
 createApp(App).mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -3,45 +3,51 @@ import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
+import Unocss from 'unocss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue({
+  plugins: [
+    vue({
     // 响应式语法糖
-    reactivityTransform: true,
-  }),
-  AutoImport({
+      reactivityTransform: true,
+    }),
+    AutoImport({
     // 配置需要自动导入的库
-    imports: [
-      'vue',
-      'vue/macros',
-      'vue-router',
-      'vue-i18n',
-      '@vueuse/core',
-      'pinia',
-      {
-        'naive-ui': [
-          'useDialog',
-          'useMessage',
-          'useNotification',
-          'useLoadingBar',
-        ],
-      },
-    ],
-    // 生成到的地址
-    dts: 'types/auto-imports.d.ts',
-    // 配置本地需要自动导入的库
-    dirs: [
+      imports: [
+        'vue',
+        'vue/macros',
+        'vue-router',
+        'vue-i18n',
+        '@vueuse/core',
+        'pinia',
+        {
+          'naive-ui': [
+            'useDialog',
+            'useMessage',
+            'useNotification',
+            'useLoadingBar',
+          ],
+        },
+      ],
+      // 生成到的地址
+      dts: 'types/auto-imports.d.ts',
+      // 配置本地需要自动导入的库
+      dirs: [
       // pinia状态管理目录
-      'src/stores',
-      // 自定义组合式api目录
-      'src/composables',
-    ],
-  }),
-  Components({
-    // 导入naiveui的配置项目
-    resolvers: [NaiveUiResolver()],
-    // 生成类型的地址
-    dts: 'types/components.d.ts',
-  })],
+        'src/stores',
+        // 自定义组合式api目录
+        'src/composables',
+      ],
+    }),
+    Components({
+      // 导入naiveui的配置项目
+      resolvers: [
+        NaiveUiResolver(),
+      ],
+      // 生成类型的地址
+      dts: 'types/components.d.ts',
+    }),
+    Unocss(),
+  ],
 })
