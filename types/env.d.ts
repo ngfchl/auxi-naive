21f6cc5790ba8454871ddfc6f3784f470e902950
ngfchl <ngfchl@126.com>
2023-03-15 14:15:13 +0800

    增加 路由类型配置

--- types/env.d.ts
+++ types/env.d.ts
@@ -1,8 +1,15 @@
 /// <reference types="vite/client" />
+import 'vue-router'
 interface ImportMetaEnv {
   readonly VITE_APP_BASE: string
 }
 
 interface ImportMeta {
   readonly env: ImportMetaEnv
+}
+
+declare module 'vue-router'{
+  interface RouteMeta {
+    title?: string // 先仅扩展一个title后续再补充
+  }
 }
