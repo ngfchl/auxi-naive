29fb8f0313fab1d82aff1800db7ed7e8a30ce8d6
ngfchl <ngfchl@126.com>
2023-03-15 14:53:56 +0800

    添加 pinia状态管理测试

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,3 +1,18 @@
+<script lang="ts" setup>
+const counterStore = useCounter()
+
+// 解构
+const { counter } = storeToRefs(counterStore)
+</script>
+
 <template>
-  <div />
+  <div>
+    值：{{ counter }}
+    <n-button @click="counterStore.increment">
+      增加
+    </n-button>
+    <n-button @click="counterStore.decrement">
+      减少
+    </n-button>
+  </div>
 </template>
---
+++ src/stores/counter.ts
@@ -0,0 +1,20 @@
+export const useCounter = defineStore('counter', () => {
+  const counter = ref(0)
+
+  const increment = () => {
+    counter.value++
+  }
+
+  const decrement = () => {
+    counter.value--
+  }
+
+  const double = computed(() => counter.value * 2)
+
+  return {
+    counter,
+    increment,
+    decrement,
+    double,
+  }
+})
