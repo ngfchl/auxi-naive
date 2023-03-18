13c708309169d9adf6f1960a5e2e0f18e0c6a32c
ngfchl <ngfchl@126.com>
2023-03-18 12:42:56 +0800

    注册登录页多语言

--- src/locales/lang/en-US.ts
+++ src/locales/lang/en-US.ts
@@ -1,7 +1,9 @@
 import { dateEnUS, enUS } from 'naive-ui'
 import global from './global/en-US'
+import pages from './pages/en-US'
 export default {
   ...global,
+  ...pages,
   naiveUI: {
     locale: enUS,
     dateLocale: dateEnUS,
