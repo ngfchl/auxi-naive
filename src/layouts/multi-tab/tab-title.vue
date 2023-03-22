80e8702c2454c3926c368ff2b4e2c7b3dd690393
ngfchl <ngfchl@126.com>
2023-03-22 13:24:16 +0800

    keepalive页面添加绿色呼吸图标

--- src/layouts/multi-tab/tab-title.vue
+++ src/layouts/multi-tab/tab-title.vue
@@ -14,18 +14,43 @@ const handleContextMenu = (e: MouseEvent) => {
 
 <template>
   <span class="flex items-center" @contextmenu="handleContextMenu">
-    <MenuIcon v-if="item.icon" :icon="item.icon" class="mr-2" />
+    <MenuIcon
+      v-if="item.icon"
+      :icon="item.icon"
+      class="mr-2"
+      :class="[item.route.meta.keepAlive ?? 'alive-animation']"
+    />
     {{ $t(item.tabTitle) }}
   </span>
   <n-icon
     v-if="current === item.path"
     class="ml-2 n-base-close n-tabs-tab__close"
-    size="18" @click="refreshTab"
+    size="18" @click="refreshTab(item.path)"
   >
     <RefreshOutline />
   </n-icon>
 </template>
 
 <style scoped>
-
+.alive-animation {
+  animation: breath infinite 5s ease;
+  color: darkgreen;
+}
+@keyframes breath {
+  0% {
+    transform: scale(0.7)
+  }
+  25% {
+    transform: scale(1.0);
+  }
+  50% {
+    transform: scale(0.8);
+  }
+  75% {
+    transform: scale(1.0);
+  }
+  100% {
+    transform: scale(0.7);
+  }
+}
 </style>
