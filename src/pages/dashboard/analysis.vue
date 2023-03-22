9cc4ed71162351ff693e27f3db01f51c5bf4b9f5
ngfchl <ngfchl@126.com>
2023-03-22 23:58:42 +0800

    在主页显示下载器速度信息

--- src/pages/dashboard/analysis.vue
+++ src/pages/dashboard/analysis.vue
@@ -1,28 +1,9 @@
 <script lang="ts" setup>
-import { colors } from '~/config/theme'
-
-const appStore = useAppStore()
-const handleTheme = (theme: string) => {
-  appStore.updateTheme(theme)
-}
-const onSwitch = (lang: string) => {
-  useAppLocale().value = lang
-}
+import Downloading from '~/pages/dashboard/components/downloading.vue'
 </script>
 
 <template>
   <n-space class="flex gap-0 justify-center" justify="space-around" size="large">
-    <n-button v-for="(item, key) in colors" :key="key" :color="item.common.primaryColor" @click="handleTheme(key)">
-      {{ key }}
-    </n-button>
-  </n-space>
-  <n-space class="mt-3">
-    <n-input />
-    <n-button @click="onSwitch('en-US')">
-      English
-    </n-button>
-    <n-button @click="onSwitch('zh-CN')">
-      中文
-    </n-button>
+    <Downloading />
   </n-space>
 </template>
