f199101c94deb4f479128b8dd6d38f039783f62d
ngfchl <ngfchl@126.com>
2023-03-22 23:57:30 +0800

    添加echarts组件

---
+++ src/components/ECharts.vue
@@ -0,0 +1,120 @@
+<script lang="ts" setup>
+import { v4 as uuidv4 } from 'uuid'
+// 引入核心组件
+import * as echarts from 'echarts/core'
+import type { BarSeriesOption, LineSeriesOption } from 'echarts/charts'
+import { BarChart, GaugeChart, LineChart } from 'echarts/charts'
+import type {
+  DatasetComponentOption,
+  GridComponentOption,
+  TitleComponentOption,
+  TooltipComponentOption,
+} from 'echarts/components'
+import {
+  DatasetComponent,
+  GridComponent,
+  LegendComponent,
+  TitleComponent,
+  ToolboxComponent,
+  TooltipComponent,
+  TransformComponent,
+} from 'echarts/components'
+import { LabelLayout, UniversalTransition } from 'echarts/features'
+import { CanvasRenderer } from 'echarts/renderers'
+
+// 通过 ComposeOption 来组合出一个只有必须组件和图表的 Option 类型
+type ECOption = echarts.ComposeOption<
+    | BarSeriesOption
+    | LineSeriesOption
+    | TitleComponentOption
+    | TooltipComponentOption
+    | GridComponentOption
+    | DatasetComponentOption
+>
+
+const props = defineProps({
+  theme: {
+    // required: true,
+    type: String,
+    default: () => (''),
+  },
+  myOption: {
+    required: true,
+    type: Object,
+    default: () => ({}),
+  },
+})
+// 注册必须的组件
+echarts.use([
+  LegendComponent,
+  ToolboxComponent,
+  TitleComponent,
+  TooltipComponent,
+  GridComponent,
+  DatasetComponent,
+  TransformComponent,
+  BarChart,
+  GaugeChart,
+  LineChart,
+  LabelLayout,
+  UniversalTransition,
+  CanvasRenderer,
+])
+const element_id = ref(uuidv4())
+
+/**
+ * 初始化图表
+ */
+const initData = () => {
+  const domNode = document.getElementById(element_id.value) as HTMLElement
+  // domNode.setAttribute('_echarts_instance_', '')
+  let myChart = echarts.getInstanceByDom(domNode)
+  if (myChart === undefined) {
+    myChart = echarts.init(
+      domNode,
+      props.theme,
+    )
+  }
+  // myChart.setOption(props.myOption)
+  // 在template中可以直接取props中的值，但是在script中不行，因为script是在挂载之前执行的
+  setTimeout(async () => {
+    myChart!.setOption(
+      props.myOption,
+      {
+        notMerge: true, // 不和之前的option合并
+      },
+    )
+  }, 300)
+
+  // 监听页面的大小
+  window.addEventListener('resize', () => {
+    setTimeout(() => {
+      myChart?.resize({
+        animation: {
+          duration: 300,
+        },
+      })
+    }, 300)
+  })
+}
+/**
+ * 页面初始化时加载数据
+ */
+onMounted(async () => {
+  initData()
+})
+/**
+ * 深度监听数据变化
+ */
+watch(() => props.myOption, (newValue, oldValue) => {
+  initData()
+}, { deep: true })
+</script>
+
+<template>
+  <div :id="element_id" style="height: 500px;width: 800px;direction: ltr;" />
+</template>
+
+<style scoped>
+
+</style>
