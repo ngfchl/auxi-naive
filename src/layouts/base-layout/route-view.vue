94136d82bcbea7305809b949d6f6435665f09360
ngfchl <ngfchl@126.com>
2023-03-22 11:44:56 +0800

    实现根据路由信息按需KeepAlive

--- src/layouts/base-layout/route-view.vue
+++ src/layouts/base-layout/route-view.vue
@@ -1,11 +1,12 @@
 <script lang="ts" setup>
-
+const router = useRouter()
 </script>
 
 <template>
   <router-view v-slot="{ Component, route }">
     <keep-alive>
-      <component :is="Component" :key="route.fullPath" />
+      <component :is="Component" v-if="route.meta.keepAlive" :key="route.fullPath" />
     </keep-alive>
+    <component :is="Component" v-if="!route.meta.keepAlive" :key="route.fullPath" />
   </router-view>
 </template>
