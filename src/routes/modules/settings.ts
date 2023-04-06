878fa77013bb26469627a5a354625cc1354902e9
ngfchl <ngfchl@126.com>
2023-04-06 19:49:48 +0800

    修复hosts页面返回错误的bug

--- src/routes/modules/settings.ts
+++ src/routes/modules/settings.ts
@@ -1,4 +1,4 @@
-const HostsSetting = () => () => import('~/pages/settings/hosts/index.vue')
+const HostsSetting = () => import('~/pages/settings/hosts/index.vue')
 const SystemSetting = () => import('~/pages/settings/system/index.vue')
 const SupervisorSetting = () => import('~/pages/settings/supervisor/index.vue')
 const FlowerSetting = () => import('~/pages/settings/flower/index.vue')
