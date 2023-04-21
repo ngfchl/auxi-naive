cf82f5447bc7710bfffa2fea64124a6e6658c347
ngfchl <ngfchl@126.com>
2023-04-22 00:53:12 +0800

    添加时间格式化方法

---
+++ src/hooks/timeFormat.ts
@@ -0,0 +1,38 @@
+export default (seconds: number) => {
+  const time = {
+    day: 0,
+    hour: 0,
+    minute: 0,
+    second: 0,
+  }
+
+  // 秒数小于1，直接返回0秒
+  if (seconds < 1)
+    return '0秒'
+
+  // 大于或等于60秒才进行计算
+  if (seconds >= 60) {
+    time.second = Math.floor(seconds % 60)
+    time.minute = Math.floor((seconds / 60) % 60)
+
+    // 大于等于60分钟才进行计算
+    if (seconds >= 3600) {
+      time.hour = Math.floor((seconds / 3600) % 24)
+
+      // 大于等于24小时才进行计算
+      if (seconds >= 86400) {
+        time.day = Math.floor(seconds / 86400)
+        return `${time.day}天${time.hour}小时`
+      }
+      else {
+        return `${time.hour}小时${time.minute}分`
+      }
+    }
+    else {
+      return `${time.minute}分${time.second}秒`
+    }
+  }
+  else {
+    return `${seconds}秒`
+  }
+}
