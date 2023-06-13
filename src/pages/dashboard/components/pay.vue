9f54643d85cdfc57436b50ece8b3be9a16b22f4a
ngfchl <ngfchl@126.com>
2023-06-14 02:02:34 +0800

    修复显示异常的BUG

--- src/pages/dashboard/components/pay.vue
+++ src/pages/dashboard/components/pay.vue
@@ -10,21 +10,19 @@
       </n-h2>
     </template>
     <n-image-group>
-      <n-space justify="center" class="-mt-6 text-13px">
+      <n-space justify="center" class="-mt-6 text-13px text-center" vertical>
         <n-image
           class="b-rounded-5 h-240px"
           src="https://api.r10086.com/%E6%A8%B1%E9%81%93%E9%9A%8F%E6%9C%BA%E5%9B%BE%E7%89%87api%E6%8E%A5%E5%8F%A3.php?%E5%9B%BE%E7%89%87%E7%B3%BB%E5%88%97=%E7%8C%AB%E5%A8%981"
           placeholder=""
         />
-        <div>
-          <p class="indent">
-            <n-image
-              class="b-rounded-1 w-100%"
-              src="https://v2.jinrishici.com/one.svg?font-size=20&spacing=2&color=darkgreen"
-              placeholder=""
-            />
-          </p>
-        </div>
+        <p class="text-center">
+          <n-image
+            class="b-rounded-1"
+            src="https://v2.jinrishici.com/one.svg?font-size=20&spacing=2&color=darkgreen"
+            placeholder=""
+          />
+        </p>
       </n-space>
     </n-image-group>
   </n-card>
